fc0ff9a7d2444a2a229ba4bc493d9dfd1ecbc9da
guy <guyskv@gmail.com>
2024-11-27 16:16:40 +0200

    Location permission

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,11 +36,15 @@ android {
 
 dependencies {
 
+
+
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(libs.ext.junit)
+
+    implementation("com.google.android.gms:play-services-location:21.3.0")
 }
